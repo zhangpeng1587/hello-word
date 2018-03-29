343def512ea4a81ab46210d1d9588711379b94cc
zhangpeng <1416077550@qq.com>
2018-03-29 16:48:52 +0800

    opt

---
+++ mytest/src/main/java/mytest/mytest/Main.java
@@ -0,0 +1,20 @@
+package mytest.mytest;
+
+public class Main {
+
+	public static void main(String[] args) {
+		main("");
+	}
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String args) {
+		extMethodTest();
+	}
+
+	private static void extMethodTest() {
+		System.out.println("main test");
+	}
+}
